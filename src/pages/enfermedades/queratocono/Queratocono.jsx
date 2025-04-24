194b31ccc04638c034b1d3915aa9aab14b62dd9c
ErikaGM19 <erika.garcia.munoz@correounivalle.edu.co>
2025-04-24 16:07:22 -0500

    HU003 - HU004 - HU006 - HU007 -HU008 -HU009 se agregamodelado 3d con texturas e iluminacion

--- src/pages/enfermedades/queratocono/Queratocono.jsx
+++ src/pages/enfermedades/queratocono/Queratocono.jsx
@@ -1,6 +1,14 @@
 import React from 'react';
 import './queratocono.css';
+import Eye from '../../home/models-3d/queratocono/Eye';
+import Light from '../../home/lights/queratocono/Lights';
+
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls } from '@react-three/drei';
 import ojoImage from '../../../assets/eye.jpg'; 
+
+
+
 function Queratocono() {
   return (
     <div className="queratocono-container">
@@ -10,26 +18,34 @@ function Queratocono() {
         <div className="info-section que-es">
           <h2 className="section-title">¿QUÉ ES?</h2>
           <p className="section-text">
-            El queratocono es una enfermedad ocular progresiva en la que la córnea, 
-            normalmente redonda, se adelgaza y comienza a abultarse hacia afuera 
-            adquiriendo forma de cono. Este cambio en la forma de la córnea puede 
-            causar visión borrosa, sensibilidad a la luz y problemas de visión 
-            que pueden afectar la calidad de vida.
+          El queratocono es una enfermedad ocular progresiva en la que la córnea 
+          se adelgaza y se deforma en forma de cono, causando visión borrosa, 
+          sensibilidad a la luz y otros problemas visuales.
           </p>
         </div>
-        
+
+
         <div className="image-keratoconus-section">
-          <img src={ojoImage} alt="Ojo con queratocono" className="eye-keratoconus-image" />
+          <Canvas shadows camera={{ position: [2, 2, 5], fov: 50 }} style={{ background: '#FFFFFF' }}>
+            <Light/>
+            <OrbitControls />
+            <Eye />
+
+            {/* Piso de la escena */}
+            <mesh receiveShadow rotation={[-Math.PI / 2, 0, 0]} position={[0, -2.5, 0]}> 
+              <planeGeometry args={[20, 20]} />
+              <meshStandardMaterial color="cyan" />
+            </mesh>
+
+          </Canvas>
         </div>
         
+
         <div className="info-section efectos">
           <h2 className="section-title">¿QUÉ EFECTOS TIENE?</h2>
           <p className="section-text">
-            Los efectos del queratocono incluyen visión distorsionada, aumento 
-            de sensibilidad a la luz y al resplandor, cambios frecuentes en la 
-            graduación de las gafas, visión borrosa o nublada, y dificultad 
-            para conducir por la noche. En casos avanzados, puede aparecer un 
-            anillo de Fleischer y estrías de Vogt en la córnea.
+          El queratocono provoca visión distorsionada, sensibilidad a la luz,
+          cambios frecuentes en la graduación, visión borrosa y dificultad para conducir de noche. 
           </p>
         </div>
       </div>
@@ -37,10 +53,10 @@ function Queratocono() {
       <div className="causa-section">
         <h2 className="section-title">¿QUÉ LA CAUSA?</h2>
         <p className="section-text">
-          Las causas exactas del queratocono son desconocidas, pero se cree que hay factores genéticos 
-          involucrados, ya que puede ser hereditario. También se asocia con frotarse excesivamente los ojos, 
-          alergias crónicas e inflamación ocular. Otros factores de riesgo incluyen ciertas condiciones como 
-          el síndrome de Down, el síndrome de Ehlers-Danlos y el asma.
+        Las causas exactas del queratocono son desconocidas, pero se cree que hay factores genéticos 
+        involucrados, ya que puede ser hereditario. También se asocia con frotarse excesivamente los ojos, 
+        alergias crónicas e inflamación ocular. Otros factores de riesgo incluyen ciertas condiciones como 
+        el síndrome de Down, el síndrome de Ehlers-Danlos y el asma.
         </p>
       </div>
     </div>
---
+++ src/pages/home/models-3d/queratocono/Eye.jsx
@@ -0,0 +1,31 @@
+import React, { useRef } from 'react';
+import { useFrame } from '@react-three/fiber';
+import { useGLTF } from '@react-three/drei';
+
+const Eye = (props) => {
+    const { nodes, materials } = useGLTF("models-3d/queratocono/keratoconus-eye.glb");
+    const eyeRef = useRef();
+
+
+    useFrame(() => {
+        if (eyeRef.current) {
+            eyeRef.current.rotation.y += 0.05;  // Rotacion
+        }
+        });
+
+    return (
+        <group {...props} dispose={null} ref={eyeRef} position={[0, 0, 0]}>
+            <mesh
+            castShadow // Sombras
+            receiveShadow
+            geometry={nodes.KeratoconusEye.geometry}
+            material={materials.KeratoconusEyeMaterial}
+            scale={7500}
+            />            
+        </group>
+    );
+};
+  
+export default Eye;
+
+useGLTF.preload("models-3d/queratocono/keratoconus-eye.glb");
